b0fd098accc18196278037b28fa0c1fa1d9e5f7a
sukhmeen-marok <isherwalmarok@gmail.com>
2026-06-21 10:11:29 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   // Force it to use the live environment variable if it exists
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
